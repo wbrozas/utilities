68c77440f358806577f0b97fcbeab6a2bfd96b87
William Brozas <wbrozas@gmail.com>
2012-10-16 13:55:13 -0400

    groupByQuantity added

--- listfuncs.hs
+++ listfuncs.hs
@@ -4,3 +4,10 @@ merge :: (Ord a) => [a] -> [a] -> [a]
 merge xs [] = xs
 merge [] ys = ys
 merge xs@(x:xs') ys@(y:ys') = if x < y then x : merge xs' ys else y : merge xs ys'
+
+groupByQuantity :: Int -> [a] -> [[a]]
+groupByQuantity _ [] = []
+groupByQuantity i xs = take i xs : groupByQuantity i (drop i xs)
+
+--selectRank :: (Ord a) => Int -> [a]
+--selectRank k xs = 
