e414be304f6a456f93befbce83a57168694f9dcb
William Brozas <wbrozas@gmail.com>
2012-10-21 20:37:36 -0400

    fix tabs

--- listfuncs.hs
+++ listfuncs.hs
@@ -14,19 +14,19 @@ groupByQuantity i xs = take i xs : groupByQuantity i (drop i xs)
 --O(n) algorithm for selection, note: you might want to optimize partition so that it deletes one occurance of mm, therefore you would need only one pass
 select :: (Ord a) => Int -> [a] -> a
 select k xs
-	| length xs <= 5 	= sort xs !! k
-	| otherwise 		= let mm = getMedianOfMedians xs 
-							in let ys = partition (< mm) xs 
-								in let size = length $ fst ys
-									in if size == k then mm else if size < k then select (k - size - 1) (delete mm $ snd ys) else select k (fst ys)
+    | length xs <= 5   = sort xs !! k
+    | otherwise        = let mm = getMedianOfMedians xs 
+                            in let ys = partition (< mm) xs 
+                                in let size = length $ fst ys
+                                    in if size == k then mm else if size < k then select (k - size - 1) (delete mm $ snd ys) else select k (fst ys)
 
 --Helper for select, splits into groups of 5 and returns the medians of those groups
 getMedianOfMedians :: (Ord a) => [a] -> a
 getMedianOfMedians xs 
-	= let ys = groupByQuantity 5 xs 
-		in let lys = last ys
-			in let y = sort lys !! (length lys `div` 2)
-				in let zs = foldr (\x acc -> sort x !! 2 : acc) [y] $ init ys 
-					in select (length zs `div` 2) zs
+    = let ys = groupByQuantity 5 xs 
+        in let lys = last ys
+            in let y = sort lys !! (length lys `div` 2)
+                in let zs = foldr (\x acc -> sort x !! 2 : acc) [y] $ init ys 
+                    in select (length zs `div` 2) zs
 
 --To come: Merge k sorted lists in O(n log k)
